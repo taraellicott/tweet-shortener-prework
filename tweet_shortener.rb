a3ad5ebe5cf3c845af9e2860f2b6b39bbfc2c05c
Tara Ellicott <taraellicott@gmail.com>
2018-04-21 20:33:51 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -16,18 +16,8 @@ def dictionary
 end
 
 
-# def word_substituter(tweet)
-#   tweet.split.collect do |word|
-#     if dictionary.keys.include?(word.downcase)
-#       word = dictionary[word.downcase]
-#     else
-#       word
-#     end
-#   end.join(" ")
-# end
-
 def word_substituter(tweet)
- x= tweet.split.map do |word|
+ x = tweet.split.map do |word|
     if dictionary.keys.include?(word.downcase)
       word = dictionary[word.downcase]
     else
@@ -36,11 +26,30 @@ def word_substituter(tweet)
   end
   x.join(" ")
 end
+
+
+# def bulk_tweet_shortener(tweets)
+#   tweets.each do |tweet|
+#     puts word_substituter(tweet)
+#   end
+# end
+
+
 def bulk_tweet_shortener(tweets)
   tweets.each do |tweet|
     puts word_substituter(tweet)
   end
 end
+
+
+# def selective_tweet_shortener(tweet)
+#   if tweet.length > 140
+#     word_substituter(tweet)
+#   else
+#     tweet
+#   end
+# end
+
 
 def selective_tweet_shortener(tweet)
   if tweet.length > 140
@@ -51,9 +60,17 @@ def selective_tweet_shortener(tweet)
 end
 
 
+# def shortened_tweet_truncator(tweet)
+#   if word_substituter(tweet).length > 140
+#     word_substituter(tweet)[0..136] + '...'
+#   else
+#     tweet
+#   end
+# end
+
 def shortened_tweet_truncator(tweet)
-  if word_substituter(tweet).length > 140
-    word_substituter(tweet)[0..136] + '...'
+  if word_substitutor(tweet).length > 140
+    word_substitutor(tweet)[0..136] + '...'
   else
     tweet
   end
