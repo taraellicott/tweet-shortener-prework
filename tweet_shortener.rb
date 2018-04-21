caee3a28a2f42a2991596026aa49d1b7b0cd0f2e
Tara Ellicott <taraellicott@gmail.com>
2018-04-21 20:21:12 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -25,15 +25,15 @@ def word_substituter(tweet_string)
 end
 binding.pry
 
-def word_substituter(tweet)
-  tweet.split.collect do |word|
-    if dictionary.keys.include?(word.downcase)
-      word = dictionary[word.downcase]
-    else
-      word
-    end
-  end.join(" ")
-end
+# def word_substituter(tweet)
+#   tweet.split.collect do |word|
+#     if dictionary.keys.include?(word.downcase)
+#       word = dictionary[word.downcase]
+#     else
+#       word
+#     end
+#   end.join(" ")
+# end
 
 def bulk_tweet_shortener(tweets)
   tweets.each do |tweet|
